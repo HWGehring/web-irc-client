3f317f0c97c03c4d0f9b9ffc8da9d1cc603f16ef
Howard Gehring <hgehring@kishmish.com>
2018-08-12 17:42:57 -0400

    Added something quick and dirty (very dirty) to enable bottom-up display of data in the chat window, added ability to add servers to local storage

--- ui/app/index.js
+++ ui/app/index.js
@@ -1,13 +1,87 @@
 global.Tether = require('tether');
 global.jQuery = require('jquery');
 
-$('#add-server-command').click(function(e) {
-    let hostname = $('#add-server-hostname').val();
-    let serverPort = $('#add-server-port').val();
-    let nickname = $('#add-server-nickname').val();
+function userEnteredText(input) {
+    $.post("http://localirc.com/api/echo", { data: input }, function(r) {
+        console.log(r);
+    });
+}
 
-    $('ul#server-list').prepend('<li class="nav-item d-flex align-items-center"><span data-feather="circle"></span>&nbsp;' + hostname + '</li>');
-    feather.replace();
+function connectToIRCServer(hostname, port, nickname) {
+    let xhr = new XMLHttpRequest();
+    let url = "http://localirc.com/api/stream?hostname=" + hostname + "&port=" + port + "&nickname=" + nickname;
+    console.log("CONNECTING: " + url);
+    xhr.open("GET", url, true);
+    xhr.onprogress = function () {
+        if (typeof xhr.previous_text === 'undefined') {
+            xhr.previous_text = '';
+        }
+        let result = xhr.responseText.substring(xhr.previous_text.length);
+        result = result.replace(/(?:\r\n|\r|\n)/g, '<br>');
+        $('#chat-log').append(result);
+        xhr.previous_text = xhr.responseText;
+    };
+
+    xhr.send();
+}
+
+$('.user-input-field').keypress(function(e) {
+    if(e.which == 13) {
+        userEnteredText($(this).val());
+    }
+});
+
+$(document).ready(function() {
+    let servers = localStorage.getItem("servers");
+    //localStorage.setItem("servers", JSON.stringify([]));
+    if (!servers) {
+        servers = [];
+        localStorage.setItem("servers", JSON.stringify(servers));
+    } else {
+        servers = JSON.parse(servers);
+    }
+
+    for (let i = 0; i < servers.length; i++) {
+        let hostnameVal = servers[i].hostname;
+        $('ul#server-list').prepend('<li class="nav-item d-flex align-items-center"><span data-feather="chevron-right"></span>&nbsp;' + hostnameVal + '</li>');
+        feather.replace();
+    }
+});
+
+$('#add-server-command').click(function(e) {
+    let hostname = $('#add-server-hostname');
+    let serverPort = $('#add-server-port');
+    let nickname = $('#add-server-nickname');
+
+    let hostnameVal = hostname.val().trim();
+    let serverPortVal = serverPort.val().trim();
+    let nicknameVal = nickname.val().trim();
+    if (hostnameVal && serverPortVal && nicknameVal) {
+        hostname.val('');
+        serverPort.val('');
+        nickname.val('');
+
+        let servers = localStorage.getItem("servers");
+
+        if (servers === null) {
+            servers = [];
+        } else {
+            servers = JSON.parse(servers);
+        }
+
+        servers.push({
+            'hostname' : hostnameVal,
+            'serverPort' : serverPortVal,
+            'nickname' : nicknameVal,
+        });
+
+        localStorage.setItem("servers", JSON.stringify(servers));
+
+        connectToIRCServer(hostnameVal, serverPortVal, nicknameVal);
+
+        $('ul#server-list').prepend('<li class="nav-item d-flex align-items-center"><span data-feather="chevron-right"></span>&nbsp;' + hostnameVal + '</li>');
+        feather.replace();
+    }
 
     $('#add-server-modal').modal('toggle');
 });
