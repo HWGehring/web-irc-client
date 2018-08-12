2cf1dd03b5d113dfa274f6cc1c4e79dd35cc73c5
Howard Gehring <hgehring@kishmish.com>
2018-08-12 03:41:52 -0400

    Adding strict compilation and implementing add server functionality

--- ui/app/index.js
+++ ui/app/index.js
@@ -1,4 +1,13 @@
 global.Tether = require('tether');
 global.jQuery = require('jquery');
 
-console.log('hello!');
+$('#add-server-command').click(function(e) {
+    let hostname = $('#add-server-hostname').val();
+    let serverPort = $('#add-server-port').val();
+    let nickname = $('#add-server-nickname').val();
+
+    $('ul#server-list').prepend('<li class="nav-item d-flex align-items-center"><span data-feather="circle"></span>&nbsp;' + hostname + '</li>');
+    feather.replace();
+
+    $('#add-server-modal').modal('toggle');
+});
