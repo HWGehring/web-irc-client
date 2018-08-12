719309ee5edb75476c2b9932b6bf1e3278a30d02
Howard Gehring <hgehring@kishmish.com>
2018-08-12 17:49:54 -0400

    Added a message queue thinking that would help with socket duplex (it didn't).
    fgets is a blocking function and production host doesn't have the resources to compile PHP, so without ZTS enabled my options are:
    
    a) re-implement fgets + mutex functionality + "data received from irc server" callback
    b) use a different language to do the async
    
    There's a pretty obvious path leading to JavaScript before the api gets its roots too deep into PHP.

--- api/src/Client.php
+++ api/src/Client.php
@@ -1,44 +1,70 @@
-<?php
+<?php namespace IRC;
 
 class Client {
 
-    private $output = null;
+    /** @var DB $db */
+    private $db = null;
 
-    public function __construct($server, $port) {
-        $fp = fsockopen($server, $port, $errno, $errstr, 30);
+    public function __construct($hostname, $port, $nickname) {
+        $this->db = DB::instance();
+
+        for ($i = 0; $i < ob_get_level(); $i++) {
+            ob_end_flush();
+        }
+
+        ob_implicit_flush(1);
+        flush();
+
+        $username = $nickname;
+
+        $fp = fsockopen($hostname, $port, $errno, $errstr, 30);
         if (!$fp) {
             echo "$errstr ($errno)" . PHP_EOL;
         } else {
-            $cmd = "USER tow0987 . . :real name\r\n";
+            $cmd = "USER {$username} . . :real name\r\n";
             fwrite($fp, $cmd);
-            echo $cmd;
-
-            $cmd = "NICK tow0987\r\n";
+            $this->dump($cmd);
+            $cmd = "NICK {$nickname}\r\n";
             fwrite($fp, $cmd);
-            echo $cmd;
-
+            $this->dump($cmd);
             $cmd = "JOIN #towchat\r\n";
             fwrite($fp, $cmd);
-            echo $cmd;
-
-            while (!feof($fp)) {
-                $response = fgets($fp, 128);
-                echo $response;
-                if (preg_match('/^PING :(.*)/', $response, $matches)) {
-                    $cmd = "PONG :{$matches[1]}\r\n";
-                    fwrite($fp, $cmd);
-                    echo $cmd;
-                }
-
-                if(!is_null($this->output)) {
-                    fwrite($fp, $cmd);
-                    echo $cmd;
-                }
-            }
-            fclose($fp);
+            $this->dump($cmd);
         }
+
+        $this->fp = $fp;
+    }
+
+    private function dump($chunk) {
+        printf("%s\r\n", $chunk);
+        flush();
+    }
+
+    public function stream() {
+        $fp = $this->fp;
+
+        while (!feof($fp)) {
+            $response = fgets($fp, 128);
+            $this->dump($response);
+            if (preg_match('/^PING :(.*)/', $response, $matches)) {
+                $cmd = "PONG :{$matches[1]}\r\n";
+                fwrite($fp, $cmd);
+                $this->dump($cmd);
+            }
+
+            $query = $this->db->query("SELECT id, payload FROM relay_queue ORDER BY created_on LIMIT 1");
+            $result = $query->fetchAll(\PDO::FETCH_ASSOC);
+            if (!empty($result)) {
+                $result = current($result);
+                $stmt = $this->db->prepare("DELETE FROM relay_queue WHERE id = :id");
+                $stmt->bindParam('id', $result['id']);
+                $stmt->execute();
+                $cmd = preg_replace("\r\n", "", $result['payload']) . "\r\n";
+                fwrite($fp, $cmd);
+                $this->dump($cmd);
+            }
+        }
+
+        fclose($fp);
     }
 }
-
-//$c = new Client("irc.lycaeum.fun", 6667);
-$c = new Client("chat.freenode.net", 6667);
--- public/index.php
+++ public/index.php
@@ -27,6 +27,38 @@ $app->get('/', function (Request $request, Response $response, array $args) {
     exit;
 });
 
+$app->post('/api/echo', function (Request $request, Response $response, array $args) {
+    $body = $request->getParsedBody();
+    if (!isset($body['data'])) {
+        echo 400;
+        exit;
+    }
+
+    try {
+        $db = IRC\DB::instance();
+        $stmt = $db->prepare("INSERT INTO relay_queue (payload) VALUES (:payload);");
+        $stmt->bindParam('payload', $body['data']);
+        $stmt->execute();
+    } catch (\Exception $e) {
+        echo 500;
+        exit;
+    }
+
+    echo 200;
+    exit;
+});
+
+$app->get('/api/stream', function (Request $request, Response $response, array $args) {
+    $params = $request->getQueryParams();
+    if (!isset($params['hostname'])) exit;
+    if (!isset($params['port'])) exit;
+    if (!isset($params['nickname'])) exit;
+
+    $client = new IRC\Client($params['hostname'], $params['port'], $params['nickname']);
+    $client->stream();
+    exit;
+});
+
 $app->get('/irc', function (Request $request, Response $response, array $args) {
     return $this->view->render($response, 'chat.html.twig');
 });
