020eaf391e2158c583123105a69f32da1b3f9ffc
Howard Gehring <hgehring@kishmish.com>
2018-08-12 19:21:01 -0400

    Removing database queue from irc client, adding documentation to JS routes

--- api/server.js
+++ api/server.js
@@ -5,6 +5,40 @@ app.get('/', function (req, res) {
     res.send('Hello World');
 });
 
+/*
+ $app->post('/api/echo', function (Request $request, Response $response, array $args) {
+ $body = $request->getParsedBody();
+ if (!isset($body['data'])) {
+ echo 400;
+ exit;
+ }
+
+ try {
+ $db = IRC\DB::instance();
+ $stmt = $db->prepare("INSERT INTO relay_queue (payload) VALUES (:payload);");
+ $stmt->bindParam('payload', $body['data']);
+ $stmt->execute();
+ } catch (\Exception $e) {
+ echo 500;
+ exit;
+ }
+
+ echo 200;
+ exit;
+ });
+
+ $app->get('/api/stream', function (Request $request, Response $response, array $args) {
+ $params = $request->getQueryParams();
+ if (!isset($params['hostname'])) exit;
+ if (!isset($params['port'])) exit;
+ if (!isset($params['nickname'])) exit;
+
+ $client = new IRC\Client($params['hostname'], $params['port'], $params['nickname']);
+ $client->stream();
+ exit;
+ });
+ */
+
 let server = app.listen(8081, function () {
     let host = server.address().address;
     let port = server.address().port;
--- api/src/Client.php
+++ api/src/Client.php
@@ -2,41 +2,53 @@
 
 class Client {
 
-    /** @var DB $db */
-    private $db = null;
-
     public function __construct($hostname, $port, $nickname) {
-        $this->db = DB::instance();
+        $this->flush();
+        try {
+            $this->fp = $this->connect($hostname, $port);
+            $this->login($nickname, $nickname);
+        } catch (\Exception $e) {
+            throw $e;
+        }
+    }
 
+    private function connect($hostname, $port) {
+        $fp = fsockopen($hostname, $port, $errno, $errstr, 30);
+        if (!$fp) {
+            throw new \Exception("$errstr ($errno)");
+        } else {
+            return $fp;
+        }
+    }
+
+    private function login($username, $nickname) {
+        $this->dump("USER {$username} . . :real name");
+        $this->dump("NICK {$nickname}");
+        $this->dump("JOIN #towchat");
+    }
+
+    private function dump($data) {
+        $this->send($data);
+        $this->echo($data);
+    }
+
+    private function send($data) {
+        $data = trim(preg_replace('/\s+/', ' ', $data));
+        return fwrite($this->fp, $data . "\r\n");
+    }
+
+    private function echo($data) {
+        $data = trim(preg_replace('/\s+/', ' ', $data));
+        printf("%s\r\n", $data);
+        flush();
+    }
+
+    public function flush() {
         for ($i = 0; $i < ob_get_level(); $i++) {
             ob_end_flush();
         }
 
         ob_implicit_flush(1);
-        flush();
-
-        $username = $nickname;
-
-        $fp = fsockopen($hostname, $port, $errno, $errstr, 30);
-        if (!$fp) {
-            echo "$errstr ($errno)" . PHP_EOL;
-        } else {
-            $cmd = "USER {$username} . . :real name\r\n";
-            fwrite($fp, $cmd);
-            $this->dump($cmd);
-            $cmd = "NICK {$nickname}\r\n";
-            fwrite($fp, $cmd);
-            $this->dump($cmd);
-            $cmd = "JOIN #towchat\r\n";
-            fwrite($fp, $cmd);
-            $this->dump($cmd);
-        }
-
-        $this->fp = $fp;
-    }
-
-    private function dump($chunk) {
-        printf("%s\r\n", $chunk);
         flush();
     }
 
@@ -48,18 +60,6 @@ class Client {
             $this->dump($response);
             if (preg_match('/^PING :(.*)/', $response, $matches)) {
                 $cmd = "PONG :{$matches[1]}\r\n";
-                fwrite($fp, $cmd);
-                $this->dump($cmd);
-            }
-
-            $query = $this->db->query("SELECT id, payload FROM relay_queue ORDER BY created_on LIMIT 1");
-            $result = $query->fetchAll(\PDO::FETCH_ASSOC);
-            if (!empty($result)) {
-                $result = current($result);
-                $stmt = $this->db->prepare("DELETE FROM relay_queue WHERE id = :id");
-                $stmt->bindParam('id', $result['id']);
-                $stmt->execute();
-                $cmd = preg_replace("\r\n", "", $result['payload']) . "\r\n";
                 fwrite($fp, $cmd);
                 $this->dump($cmd);
             }
